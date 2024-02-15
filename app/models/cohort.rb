0309f8600ccb00fee672f4804aa22baf8681f703
Matt Enyeart <menyeart1@gmail.com>
2024-02-15 10:42:11 -0700

    Creat number of students and status methods

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,4 +3,12 @@ class Cohort < ApplicationRecord
 
    validates :name, uniqueness: true, presence: true
    validates_presence_of :start_date, :graduation_date
+
+   def number_of_students
+      self.students.count
+   end
+
+   def status
+      self.graduation_date < Date.today ? "graduated" : "current"
+   end
 end 
